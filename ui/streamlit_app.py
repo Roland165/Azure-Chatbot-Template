acbd5001d9a0466c89ff7e03a30aabe431042866
Roland <roland.fontanes@gmail.com>
2025-11-04 17:44:39 +0100

    changes streamlit app

--- ui/streamlit_app.py
+++ ui/streamlit_app.py
@@ -9,27 +9,10 @@ from app.rag import answer
 import streamlit as st
 
 
-# logo
-st.set_page_config( page_title="Ateme Chatbot")
-
-if st.get_option('theme.base') == "dark":
-    logo_path = os.path.join(os.path.dirname(__file__), "logo.png")
-else :
-    logo_path = os.path.join(os.path.dirname(__file__), "logo-modified.png")
-
-
-from PIL import Image
-
-
-logo = Image.open(logo_path)
-st.image(logo, width=180)
-
-
-
-st.title("AtemeChat" )
+st.title("My Chatbot" )
 
 # answer part
-q = st.text_input( 'Quelle est ta question sur Ateme ou le stage')
+q = st.text_input( 'What is your question : ')
 if  st.button("Ask" ) or q:
     with st.spinner(' Processing...'):
         res = answer(q )
